1f9195b0643b341ba723a229f19513d79d72709f
Birgit <birgit.bartenschlager@web.de>
2024-01-14 17:05:26 +0100

    implement text to speech

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 // import TextToSpeech from "./TextToSpeech";
-
 // import TextTranslation from "./TextTranslation";
 // import TextClassification from "./TextClassification";
 // import TextGeneration from ".'/TextGeneration";
--- src/TextToSpeech.tsx
+++ src/TextToSpeech.tsx
@@ -1,5 +1,28 @@
+import { HfInference } from "@huggingface/inference";
+import { useEffect, useState } from "react";
+
 const TextToSpeech = () => {
-  return <div>TextToSpeech</div>;
+  const [speechUrl, setSpeechUrl] = useState<string>();
+  const token = import.meta.env.VITE_HF_TOKEN;
+
+  const textToSpeech = "I finally managed to do this.";
+
+  useEffect(() => {
+    const hf = new HfInference(token);
+    hf.textToSpeech({
+      inputs: textToSpeech,
+      model: "espnet/kan-bayashi_ljspeech_vits",
+    })
+      .then((res) => URL.createObjectURL(res))
+      .then((data) => setSpeechUrl(data))
+      .catch((err) => console.log(err));
+  });
+
+  return (
+    <div>
+      <audio src={speechUrl} controls></audio>
+    </div>
+  );
 };
 
 export default TextToSpeech;
